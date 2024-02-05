138309152c8641c4cc291012a219c7bdedaad9bb
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-02-05 16:52:29 +0000

    coding question

--- kickoff warmup.py
+++ kickoff warmup.py
@@ -1,3 +1,3 @@
 # This is my answer 
-So i think a computer program is like a program that do coding and makes software designs and different types of website like gaming websites or websites to find a job 
-MY favorite food is fried chicken and the way to cook it and you got to cut up four pound chicken vegatable oil 1 teaspoon paparika sat and pepper to taste and 2 cups of flour.
+So i think a computer program is like a program that do different types of coding, and makes software designs, and different types of website like gaming websites, or websites to find a job 
+MY favorite food is fried chicken and the way to cook it and you got to cut up four pound chicken, vegatable oil, 1 teaspoo,n paparika, sat and pepper to taste, and 2 cups of flour.
