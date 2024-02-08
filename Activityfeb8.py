4e52b7ceeb01743d20d780d1ff6e9fec0fe90236
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-02-08 17:35:17 +0000

    lesson

---
+++ Activityfeb8.py
@@ -0,0 +1,40 @@
+# Using the tools we've learned in class 
+# up to this point create blocks of code that will return a value.
+#(you can use variables, data types, operators, etc.)
+
+# 1. Create a block of code that uses the any of the arithmatic 
+# operator and then print out the result.
+
+# 2. Create a block of code that uses the AND operator and and then
+# print out the result.
+# always transform data into string 
+# 3. Create a block of code that uses the NOT EQUAL operator and then 
+# use then print out the result
+number_A=int(input(enter a number 
+number_a=int(input(enter another number ))  
+#2 create a block of that uses the 
+AND operator and and then print out the results 
+
+
+
+
+
+
+
+
+
+
+
+
+fruit_B,onranges 
+fruit_A'apples    
+
+print(fruit_A!=fruit_B)
+
+#(double equal sign)=comparing if
+#values are the same
+
+
+#!=(exclaimation and equal sign)=comparing
+#if values are not the same #
+            ))
