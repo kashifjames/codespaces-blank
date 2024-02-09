e063714838c059dd90baff31bfe7a3bb91944d8a
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-02-09 17:56:28 +0000

    lesson.py

---
+++ lesson feb9.py
@@ -0,0 +1,8 @@
+user_number1=int(input(enter a number))
+User number2=int(input(enter a number 
+user number3=int(input a number))
+
+
+userName_input(username,)
+userShoesize;;=(username)
+useraAge                       ))
---
+++ lesson.py
@@ -0,0 +1,30 @@
+print()
+input()
+age=13
+print(hi i am (age)years old))
+print()
+print(testNumber)
+
+
+
+print(hi,i am ,+ str (age+years old ) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+datatype=string:
+
+
+
+
+
+print(hi Iam (age years old ))
