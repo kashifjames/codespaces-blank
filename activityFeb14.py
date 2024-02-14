728bb0ce231781daa6cff6101bad5a1450c5ac39
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-02-14 17:52:16 +0000

    activityFeb14.py

---
+++ activityFeb14.py
@@ -0,0 +1,14 @@
+
+print(ingredinants, penutbuter jelly 2 slices of bread
+print(step 2 spread penutbutter on slice of bread 
+print step spread jelly on the slice of bread 
+print(step 4 put both slices together with the penutbutter and jelly facing each other 
+      
+def cheese(
+
+print(cooking 
+print 
+print
+print(step 3 salt
+      
+))
