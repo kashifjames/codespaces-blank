eda74079a0f71a892a21bac471795129dc66ace5
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-02-20 17:26:51 +0000

    quiz1.py

---
+++ activityFeb15.py
@@ -0,0 +1,11 @@
+print(type(100,000)
+
+def recipes
+# some codes were saving for later))
+3 different types of bulit in functions are print( ) function.
+type( ) function.
+input( ) function.
+abs( ) function.
+pow( ) function.
+dir( ) function.
+sorted( ) function.
