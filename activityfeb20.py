581067446613891168436a9f1c3444fb93e3f6a2
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-02-21 17:53:40 +0000

    questionsfeb21.py

---
+++ activityfeb20.py
@@ -0,0 +1,3 @@
+Based on the video you just watched, in your own words, describe what is the Socratic Method.I think socratic method is nstructor of a law class asks a student to summarize the facts of a specific court case.
+
+Why do you think it is important to asks questions in software engineering? Because you could learn more about it and probaley master 
