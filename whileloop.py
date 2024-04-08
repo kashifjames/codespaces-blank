01c8235a80a4606b597289a6e017747ea26bf116
kashifjames <158080846+kashifjames@users.noreply.github.com>
2024-04-08 15:28:56 +0000

    async work april_8th

---
+++ whileloop.py
@@ -0,0 +1,18 @@
+ I think python loops are repeating something over and over until a particular condition is satisfied. Ai = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+
+  i += 
+  i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+  i = 1
+ i < 6
+
+  print(i)
+  i += 1
